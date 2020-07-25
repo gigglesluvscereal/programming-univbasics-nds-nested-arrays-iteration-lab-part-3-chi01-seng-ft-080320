d07bcef0bc8ffeabfaeef816f7d75a86c2877031
David Gonzales <dsg2386@gmail.com>
2020-07-25 17:46:03 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,11 +1,6 @@
 def join_nested_strings(src)
   # src will be an Array of Arrays of Strings and Integers
   # Combine all Strings present in the AoA into a single value and return it
-  array = []
-  counter = 0 
-  while counter < src.length 
-  array.push ("The quick brown fox studied Ruby")
-  counter += 1 
-end
-array
+mixed_date_1 = [ ["The", 4, "quick"], [-1, "brown", "fox", 30], ["studied", 101, 233, "Ruby"]
+]]
 end
